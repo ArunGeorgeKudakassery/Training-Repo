821a43f9f6937f463d3203709691a3a9f1041df5
Arun George <arun.george@valuelabs.com>
2021-12-31 13:10:42 +0530

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { TestComponentComponent } from './Modules/test-component/test-component.component';
+import { SharedModule } from './shared/shared.module';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TestComponentComponent
+  ],
+  imports: [
+    BrowserModule,
+    SharedModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
